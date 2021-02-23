bfca5a6ab06f535cb3e34d1f039f32e5cf41a6b1
marko9988 <markosamardzic.kruscic@gmail.com>
2021-02-23 14:41:12 +0100

    Ubacivanje prve sekcije (Header)
    Ubacivanje prve header sekcije po uzoru na sliku dizajna sajta

--- index.php
+++ index.php
@@ -30,6 +30,115 @@
     <link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 <body>
+	<!--------------HEADER SAMARDZIC--->
+	<div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <div class="humberger-menu-overlay"></div>
+    <div class="humberger-menu-wrapper">
+        
+        </div>
+    </div> 
+    
+    <header class="header-section">
+        <div class="ht-options">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-8">
+                        <div class="ht-widget">
+                            <ul>
+                                <li><i class="fa fa-users"></i><span>Server Status Players: <span>247/1000</span></span> -Online</li>
+                                <li><i class="fa fa-server"></i><span>Server IP: </span> samp.bu-rp.com:777</li>
+                                
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-4">
+                        <div class="ht-social">
+                          <a href="#"><img src="instagram-brands.png"></a>
+                          <a href="#"><img src="facebook-square-brands.png"></a>
+                          <a href="#"><img src="discord-brands.png"></a>
+                           
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="logo">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                       <a href="./index.html"><img src="logo.png" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <div class="banner" style="background-image: url(background.png);"></div>
+                    <div class="carousel-caption">
+                        <h2 class="animated bounceInRight" style="animation-delay: 1s">STARTUJTE VAŠU IGRU</h2>
+                        <h3 class="animated bounceInLeft" style="animation-delay: 2s">Zaigrajte sa vašim prijateljima, posetite našu zajednicu i pridružite nam se na potpunu novom gamemodu. Kod BU:RP zajednice ne postoji limit i prepuštamo vam totalnu kontrolu u igri. Jedina prepreka je vaša mašta!</h3>
+                        <p class="animated bounceInRight" style="animation-delay: 3s" class="button_example"><a href="#">Download</a></p>
+                        <p class="animated zoomIn" style="animation-delay: 3s" class="button_example"><a href="#">Forum</a></p> 
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="banner" style="background-image: url(background.png);"></div>
+                    <div class="carousel-caption">
+                        <h2 class="animated slideInDown" style="animation-delay: 1s">STARTUJTE VAŠU IGRU</h2>
+                        <h3 class="animated fadeInUp" style="animation-delay: 2s">Zaigrajte sa vašim prijateljima, posetite našu zajednicu i pridružite nam se na potpunu novom gamemodu. Kod BU:RP zajednice ne postoji limit i prepuštamo vam totalnu kontrolu u igri. Jedina prepreka je vaša mašta!</h3>
+                        <p class="animated zoomIn" style="animation-delay: 3s"><a href="#">Download</a></p>
+                         <p class="animated zoomIn" style="animation-delay: 3s"><a href="#">Forum</a></p>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="banner" style="background-image: url(background.png);"></div>
+                    <div class="carousel-caption">
+                        <h2 class="animated zoomIn" style="animation-delay: 1s">STARTUJTE VAŠU IGRU</span></h2>
+                        <h3 class="animated fadeInLeft" style="animation-delay: 2s">Zaigrajte sa vašim prijateljima, posetite našu zajednicu i pridružite nam se na potpunu novom gamemodu. Kod BU:RP zajednice ne postoji limit i prepuštamo vam totalnu kontrolu u igri. Jedina prepreka je vaša mašta!</h3>
+                        <p class="animated zoomIn" style="animation-delay: 3s"><a href="#">Download</a></p>
+                        <p class="animated zoomIn" style="animation-delay: 3s"><a href="#">Forum</a></p>
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Prethodna</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Sledeća</span>
+            </a>
+        </div>
+    </header>
+       
+    <!--------------ABOUT US NEMANJA--->
+
+    <!--------------DONATORI CORAK--->
+
+    <!--------------FOOTER CIZMAR--->
+
+
+    <!-----------JavaScript pozivanje ksripti--->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/circle-progress.min.js"></script>
+    <script src="js/jquery.barfiller.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </body>
 </html>
