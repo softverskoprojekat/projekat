2bddd57d656162ef85d5705f2909c0e3024bd0f3
nemanjam9 <37067875+nemanjam9@users.noreply.github.com>
2021-03-02 15:54:18 +0100

    Ubacena about us sekcija sa svim njenim komponentama
    Ubacena about us sekcija sa pratecim materijalom (Background slika, ikonice neophodne za animiranje padajuceg menija kao i prateci css i JS fajl)

--- index.php
+++ index.php
@@ -28,8 +28,11 @@
     <link rel="stylesheet" href="css/header_magnific-popup.css" type="text/css">
     <link rel="stylesheet" href="css/header_slicknav.min.css" type="text/css">
     <link rel="stylesheet" href="css/header_style.css" type="text/css">
-
+    
     <link rel="stylesheet" type="text/css" href="footer_style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/aboutus_style.css">
 </head>
 <body>
 	<!--------------HEADER SAMARDZIC--->
@@ -124,6 +127,71 @@
     </header>
        
     <!--------------ABOUT US NEMANJA--->
+    <section id="aboutus">
+    <div class="album py-5 bg-light bg-image" style="background-image: url('images/aboutus_bg.jpg');">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              <div class="card card-color shadow-sm">    
+                <div class="card-header card-header-text text-center">
+                    <h4 class="my-0 fw-normal">O NAMA</h4>
+                  </div>
+                <div class="card-body">
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ducimus assumenda commodi ab quam optio labore ad magnam ut, at deleniti vitae numquam similique illo sunt consequatur! Laboriosam, molestiae debitis voluptate mollitia quod facere cumque quas quo repellendus excepturi aut ullam saepe nisi dolor reiciendis, autem aliquam ab? Quasi at est a iusto ut, veritatis quidem natus dolorum quod praesentium, harum quam? Porro repellendus aperiam dolor dolorum cum nulla eum, nesciunt quibusdam adipisci odio ea necessitatibus aliquid sunt doloremque. Suscipit nemo corporis possimus, labore aliquid incidunt? Veritatis aspernatur tempore voluptate! Asperiores ipsam enim eligendi voluptatum porro quia libero voluptatibus repellendus.</p>    
+                </div> 
+            </div>
+            </div>
+            <div class="col-sm">
+              <div class="card card-color shadow-sm">
+                <div class="card-header card-header-text text-center">
+                    <h4 class="my-0 fw-normal">ADMINISTRACIJA</h4>
+                  </div>
+                  <div class="card-body card-text">
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                      <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                       OWNERI
+                        </button>
+                      </h2>
+                      <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                          <strong>This is the first item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                      </div>
+                    </div>
+                    <div class="accordion-item">
+                      <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                          HEADMASTERI
+                        </button>
+                      </h2>
+                      <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                          <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                      </div>
+                    </div>
+                    <div class="accordion-item">
+                      <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                          HEADADMINI
+                        </button>
+                      </h2>
+                      <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                          <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
     <!--------------DONATORI CORAK--->
 
@@ -190,7 +258,7 @@
         </div>
     </section>
 
-    <!-----------JavaScript pozivanje ksripti--->
+    <!-----------JavaScript pozivanje skripti--->
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.magnific-popup.min.js"></script>
@@ -201,6 +269,8 @@
     <script src="js/main.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-----ABOUT US JAVASCRIPT--->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
 </body>
 </html>
